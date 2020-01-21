df0fddab2244e54b3c748b61f20c027c80166b3f
vivekgondalia <vgondalia@drintl.com>
2020-01-21 17:28:47 -0500

    refactor

--- data-scrapping.py
+++ data-scrapping.py
@@ -5,15 +5,18 @@ from bs4 import BeautifulSoup
 url = requests.get('https://www.cicnews.com/feed')
 coverpage = url.content
 
+soup = BeautifulSoup(coverpage, 'html5lib')
+articles = soup.find_all('item')
+
+print(articles[0].find('title'))
+
+
 #load the data into bs4
 # soup = BeautifulSoup(data.text, 'html.parser')
-soup = BeautifulSoup(coverpage, 'html5lib')
 
 # table = soup.find('table', {'class' : 'd3-o-table--row-striping'})
 # tbody = table.find('tbody')
 
-articles = soup.find_all('item')
-print(articles[0].find('title'))
 # matchup_items = tbody.findAll('tr')
 # for item in matchup_items:
 #     team_rank = item.find('p', {'class' : 'rank'}).text.strip()
